ea7c0dc871d13e2993c38021822c1f3f4b0ad00a
Naibuu <hawkwer141099@gmail.com>
2023-02-19 14:20:13 +0200

    Placeholder landing page

---
+++ pages/home/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import { HomeStyled } from "./styles";
+
+export default function Home() {
+  return (
+    <>
+      <HomeStyled>
+        <p>Hello</p>
+      </HomeStyled>
+    </>
+  );
+}
---
+++ pages/home/styles.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const HomeStyled = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+width: 100%;
+height: 100vh;
+
+& > p {
+    font-size: 5rem;
+    color: red;
+}
+`
