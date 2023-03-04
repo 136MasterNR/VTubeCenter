6201fd45f58d87ff1f1f3bc1264886e1f9cde4be
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-03-04 23:57:11 +0200

    Example Page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,13 +1,42 @@
+import Link from 'next/link';
 import Head from "next/head";
-import Image from "next/image";
+import Image from 'next/image';
 import { HomeStyled } from "./styles";
+import data from '../../public/vtubers.json';
+import { useState } from 'react';
 
-export default function Home() {
+interface Props {
+  img: string;
+}
+
+function CreateVtuber(props: Props) {
+
+  return <Image
+    src={`/img/${props.img}.webp`}
+    width={144}
+    height={144}
+    alt=""
+  />
+}
+
+
+/* Index  */
+export default function Test() {
+
   return (
     <>
-      <HomeStyled>
-        <p>Hello</p>
-      </HomeStyled>
+    <Head>
+      <title>Virtual Doggirls</title>
+      <meta content="Virtual Doggirls" property="og:title"/>
+    </Head>
+    <HomeStyled>
+      {data.doggirls.map((item) => (
+        <li key={item.username}>
+          <h2>{item.username}</h2>
+          <CreateVtuber img={item.username}/>
+        </li>
+      ))}
+    </HomeStyled>
     </>
   );
 }
--- pages/home/styles.ts
+++ pages/home/styles.ts
@@ -1,15 +1,30 @@
 import styled from "@emotion/styled";
 
 export const HomeStyled = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: center;
-width: 100%;
-height: 100vh;
 
-& > p {
-    font-size: 5rem;
-    color: red;
+text-align: center;
+padding-top: 15px;
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(calc(100% / 3 - 20px), 1fr));
+border-bottom: 1px solid white;
+border-left: 1px solid white;
+border-right: 1px solid white;
+border-bottom-left-radius: 1pc;
+border-bottom-right-radius: 1pc;
+
+img {
+    line-height: 110px;
+    text-align: center;
+    color: mediumpurple;
+    display: inline-block;
+    cursor: pointer;
+    width: 128px;
+    height: 128px;
+    min-width: 128px;
+    min-height: 128px;
+    border-radius: 2px;
+    border: 4px solid white;
+    border-radius: 50%;
 }
+
 `
