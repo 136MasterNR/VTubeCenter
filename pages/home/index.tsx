29c830dce6ab88906109ec9ee20952ded7b2fe91
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-06-24 13:11:17 +0300

    sheesh

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,41 +1,45 @@
 import Link from 'next/link';
 import Head from "next/head";
 import Image from 'next/image';
-import { HomeStyled } from "./styles";
+import { HomeStyled } from './styles';
 import data from '../../public/vtubers.json';
 import { useState } from 'react';
 
 interface Props {
-  img: string;
+  username: string;
+  name: string;
 }
 
-function CreateVtuber(props: Props) {
-
-  return <Image
-    src={`/img/${props.img}.webp`}
-    width={144}
-    height={144}
-    alt=""
-  />
-}
+function CreateVtuber(props: Props) { return (
+  <li className='vtuber'>
+    <div className='img'>
+      <Image
+        src={`/img/${props.username}.webp`}
+        width={144}
+        height={144}
+        alt={`${props.username}`}
+      />
+    </div>
+    <div className='details'>
+      {props.name}
+    </div>
+  </li>
+);}
 
 /* Index */
 export default function Test(): JSX.Element {
   return (
     <>
-    <Head>
-      <title>Virtual Doggirls</title>
-      <meta content="Virtual Doggirls" property="og:title"/>
-    </Head>
-    <h1 style={{textAlign: "center"}}>Virtual Doggirls</h1>
-    <HomeStyled>
-      {data.doggirls.map((item) => (
-        <li key={item.username}>
-          <h2>{item.username}</h2>
-          <CreateVtuber img={item.username}/>
-        </li>
-      ))}
-    </HomeStyled>
+      <Head>
+        <title>Virtual Doggirls</title>
+        <meta content="Virtual Doggirls" property="og:title" />
+      </Head>
+      <h1 style={{ textAlign: "center" }}>Virtual Doggirls</h1>
+      <HomeStyled>
+        {data.doggirls.map((item) => (
+          <CreateVtuber key={item.username} username={item.username} name={item.name} />
+        ))}
+      </HomeStyled>
     </>
   );
 }
--- pages/home/styles.ts
+++ pages/home/styles.ts
@@ -1,30 +1,30 @@
 import styled from "@emotion/styled";
-
 export const HomeStyled = styled.div`
 
-text-align: center;
-padding-top: 15px;
-display: grid;
-grid-template-columns: repeat(auto-fit, minmax(calc(100% / 3 - 20px), 1fr));
-border-bottom: 1px solid white;
-border-left: 1px solid white;
-border-right: 1px solid white;
-border-bottom-left-radius: 1pc;
-border-bottom-right-radius: 1pc;
+display: flex;
+flex-wrap: wrap;
+flex-direction: row;
+justify-content: space-between;
+gap: 10px;
 
-img {
-    line-height: 110px;
-    text-align: center;
-    color: mediumpurple;
-    display: inline-block;
-    cursor: pointer;
-    width: 128px;
-    height: 128px;
-    min-width: 128px;
-    min-height: 128px;
-    border-radius: 2px;
-    border: 4px solid white;
-    border-radius: 50%;
+.vtuber {
+  width: 350px;
+  border: solid 1px black;
 }
 
-`
+img {
+  line-height: 110px;
+  text-align: center;
+  color: mediumpurple;
+  display: inline-block;
+  cursor: pointer;
+  width: 128px;
+  height: 128px;
+  min-width: 128px;
+  min-height: 128px;
+  border-radius: 2px;
+  border: 4px solid white;
+  border-radius: 12.5%;
+}
+
+`;
