659d0579a8ed4fcb947b5b8abe8fa5ee192a78f4
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-06-26 03:47:13 +0300

    Shiki Miyoshino becoming a Star

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={fontPrimary.className}>{children}</body>
+      <body className={fontPrimary.className}>{children}
+      <div className="bg"></div>
+      </body>
     </html>
   )
 }
