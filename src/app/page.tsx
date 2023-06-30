24c69c9fda051f7b8b1c9aded321dac0688856d4
Naibuu <hawkwer141099@gmail.com>
2023-06-30 13:38:36 +0300

    crazy!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,107 +1,8 @@
 import '../scss/home.scss'
 
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import VTubers from '../data/vtubers'
+import { VTuber } from '@/common/VTuber'
 
-import Icon from '../common/Icon'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBuildingUser } from '@fortawesome/free-solid-svg-icons'
-
-import { Poppins } from 'next/font/google'
-import { Noto_Sans_JP } from 'next/font/google'
-const fontPoppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
-const fontJP = Noto_Sans_JP({
-  subsets: ['latin'],
-  weight: ['400', '500', '600'],
-})
-
-interface ISocial {
-  icon: IconProp
-  url: string
-}
-
-interface IVtuberProps {
-  name: string
-  username: string
-  description: string
-  language: string
-  affiliation: string
-  affiliationURL?: string
-  social_media: ISocial[]
-  colorScheme?: string
-}
-
-function CreateVtuber(props: IVtuberProps) {
-  const {
-    name,
-    username,
-    description,
-    language,
-    affiliation = "Independent",
-    affiliationURL,
-    social_media,
-    colorScheme = "45, 44, 63",
-  } = props
-
-  return (
-    <div className="vtuber" style={{
-      background: `linear-gradient(45deg, rgba(${colorScheme}, 0.15) 0%,rgba(255, 255, 255, 0.15) 100%),rgb(45, 44, 63)`
-    }}>
-      <div className="header">
-        <img
-          src={`/img/header/${username}.webp`}
-          alt={username}
-        />
-      </div>
-
-      <div className="avatar">
-        <img
-          src={`/img/avatar/${username}.webp`}
-          alt={username}
-        />
-      </div>
-
-      <div className="language" style={{
-        backgroundColor: `rgba(${colorScheme}, 1)`
-      }}>{language}</div>
-
-      <div className="social_media">
-        {social_media.map((media, index) => (
-          <Icon
-            key={index}
-            icon={media.icon}
-            remote={media.url}
-            size={26}
-          />
-        ))}
-      </div>
-
-      <div className={`name ${fontJP.className}`}>
-        {name}
-      </div>
-
-      <div className={`affiliation ${fontJP.className}`}>
-        {affiliation !== 'Independent' ? (
-          <a href={affiliationURL} target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon
-              icon={faBuildingUser}
-              width={24}
-              height={24}
-            />
-            {affiliation}
-          </a>
-        ) : null}
-      </div>
-
-      <div className="description">
-        {description}
-      </div>
-    </div>
-  )
-}
 
 export default function Home(): JSX.Element {
   return (
@@ -110,7 +11,7 @@ export default function Home(): JSX.Element {
 
       <main className="list">
         {VTubers.map((vtuber) => (
-          <CreateVtuber key={vtuber.username} {...vtuber}/>
+          <VTuber key={vtuber.username} {...vtuber}/>
         ))}
       </main>
     </div>
---
+++ src/common/VTuber.tsx
@@ -0,0 +1,85 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+
+import Icon from '../common/Icon'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBuildingUser } from '@fortawesome/free-solid-svg-icons'
+
+import { Noto_Sans_JP } from 'next/font/google'
+const fontJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+})
+
+interface ISocial {
+  icon: IconProp
+  url: string
+}
+
+interface IProps {
+  name: string
+  username: string
+  description: string
+  language: string
+  affiliation: string
+  affiliationURL?: string
+  social_media: ISocial[]
+  colorScheme?: string
+}
+
+export const VTuber = (props: IProps) => {
+  const {
+    name,
+    username,
+    description,
+    language,
+    affiliation = 'Independent',
+    affiliationURL,
+    social_media,
+    colorScheme = '45, 44, 63',
+  } = props
+
+  return (
+    <div
+      className="vtuber"
+      style={{
+        background: `linear-gradient(45deg, rgba(${colorScheme}, 0.15) 0%,rgba(255, 255, 255, 0.15) 100%),rgb(45, 44, 63)`,
+      }}
+    >
+      <div className="header">
+        <img src={`/img/header/${username}.webp`} alt={username} />
+      </div>
+
+      <div className="avatar">
+        <img src={`/img/avatar/${username}.webp`} alt={username} />
+      </div>
+
+      <div
+        className="language"
+        style={{
+          backgroundColor: `rgba(${colorScheme}, 1)`,
+        }}
+      >
+        {language}
+      </div>
+
+      <div className="social_media">
+        {social_media.map((media, index) => (
+          <Icon key={index} icon={media.icon} remote={media.url} size={26} />
+        ))}
+      </div>
+
+      <div className={`name ${fontJP.className}`}>{name}</div>
+
+      <div className={`affiliation ${fontJP.className}`}>
+        {affiliation !== 'Independent' ? (
+          <a href={affiliationURL} target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faBuildingUser} width={24} height={24} />
+            {affiliation}
+          </a>
+        ) : null}
+      </div>
+
+      <div className="description">{description}</div>
+    </div>
+  )
+}
