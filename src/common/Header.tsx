fc7f171e9465cd0b37e22a3c4536c67008a784da
Naibuu <hawkwer141099@gmail.com>
2023-07-03 17:26:58 +0300

    woah brand new header layout

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -5,7 +5,7 @@ import '../scss/header.scss'
 import React, { useEffect, useState } from 'react'
 import { Search } from './Filter/Search'
 
-interface HeaderProps {
+interface IProps {
   queryFilter: (query: string) => void
   directory?: any
   children?: React.ReactNode
@@ -22,23 +22,20 @@ function getPath() {
   return Dir
 }
 
-export const Header = (props: HeaderProps) => {
-  const { directory = getPath() } = props
+export const Header = (props: IProps) => {
+  const { directory = getPath(), queryFilter } = props
 
   return (
     <div>
       <header>
-        <div className='bg'></div>
-        <div className='items'>
-          <div className='left'>
+        <div className="header-content">
+          <div className="header-branding">
             <img src="/favicon.ico" width={36} height={36} />
             <p>/ {directory}</p>
           </div>
-          <div className='middle'>
-            <Search queryFilter={props.queryFilter}/>
-          </div>
-          <div className='right'>
-            <div className='img'></div>
+          <div className="searchbar-content">
+            <Search queryFilter={queryFilter} />
+            <div className="searchbar-foreground"></div>
           </div>
         </div>
       </header>
