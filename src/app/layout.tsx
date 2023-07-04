d24b43c2c35f5609f1e955bccdf7872aa0a7e38d
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-07-04 20:01:33 +0300

    fixed my self-made issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,40 @@
-"use client"
+export const metadata = {
+  title: 'VTube Center',
+  description: 'Get a list of every anime Virtual Doggirl in the world!',
+  keywords: [
+    'virtual',
+    'doggirls',
+    'dog',
+    'girls',
+    'anime',
+    'list',
+    'online',
+    'media',
+    'link',
+    'video',
+    'image',
+    'live',
+    'stream',
+    'livestream',
+  ],
+  google: 'notranslate',
+  rating: 'general',
+  googleSiteVerification: 'u_JknkJF5JDYvj-PzNGwOc2XQ0XRfryIgr8PeF0y3Vc',
+  openGraph: {
+    type: 'website',
+    locale: 'en-US',
+    title: 'VTube Center',
+    description: 'Get a list of every anime Virtual Doggirl in the world!',
+  },
+  twitter: {
+    images:
+      'https://github.com/136MasterNR/VTubeCenter/raw/migration/app/public/img/seo/embed.jpg',
+    title: 'VTube Center',
+    card: 'summary_large_image',
+    creator: '@136MasterNR and @Naibuu',
+  },
+}
 
-import { useState, useEffect } from 'react';
-
-import './metadata'
 import '../scss/globals.scss';
 import Loading from './loading'
 
@@ -10,35 +42,11 @@ import { Plus_Jakarta_Sans } from 'next/font/google'
 const fontPrimary = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 750);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  useEffect(() => {
-    if (!loading) {
-      const loadingElement = document.getElementById('loading');
-      if (loadingElement) {
-        const animationEndHandler = () => {
-          loadingElement.style.display = 'none';
-          loadingElement.removeEventListener('animationend', animationEndHandler);
-        };
-
-        loadingElement.addEventListener('animationend', animationEndHandler);
-        loadingElement.classList.add('loading-fade-out');
-      }
-    }
-  }, [loading]);
 
   return (
     <html lang="en">
       <body className={fontPrimary.className}>
-        <Loading loading={loading}/>
+        <Loading/>
         {children}
         <div className="bg"></div>
       </body>
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,35 @@
+"use client"
+
 import '../scss/loading.scss';
 
-export interface LoadingProps {
-  loading: boolean;
-}
+import { useState, useEffect } from 'react';
 
-export default function Loading({ loading }: LoadingProps) {
+export default function Loading() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 750);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      const loadingElement = document.getElementById('loading');
+      if (loadingElement) {
+        const animationEndHandler = () => {
+          loadingElement.style.display = 'none';
+          loadingElement.removeEventListener('animationend', animationEndHandler);
+        };
+
+        loadingElement.addEventListener('animationend', animationEndHandler);
+        loadingElement.classList.add('loading-fade-out');
+      }
+    }
+  }, [loading]);
+
   return (
     <div id="loading" className={`loading ${loading ? '' : 'loading-active'}`}>
       Loading...
