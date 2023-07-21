7cb6a4d6b13cce234efac7198007a0e389a0e69e
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-07-21 15:42:06 +0300

    home update, added every single model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,11 @@ export default function Home() {
             </Link>
           </div>
         </div>
-        <div className='showcase'></div>
+        <div className='showcase'>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
         <svg className='bg-blur' width="589" height="785" viewBox="0 0 589 785" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M313.77 58.4608C89.277 -129.575 11.5868 183.403 0.80342 363.397C107.242 1010.89 549.388 731.46 578.054 685.91C606.72 640.359 594.387 293.506 313.77 58.4608Z" fill="white"/>
         </svg>
